d846d2a7f73de051eaa04465b61bf75089df13cb
alfred-zhong <kqzhong@gmail.com>
2020-04-27 09:39:19 +0800

    add comment

--- metrics.go
+++ metrics.go
@@ -4,6 +4,8 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+// AddCounter creates a new Counter based on the provided CounterOpts which
+// will be registered automatically when client serves.
 func (c *Client) AddCounter(opts prometheus.CounterOpts) prometheus.Counter {
 	opts = prometheus.CounterOpts(
 		formatOpts(prometheus.Opts(opts), c.ConstLabels, c.ServiceName),
@@ -14,6 +16,9 @@ func (c *Client) AddCounter(opts prometheus.CounterOpts) prometheus.Counter {
 	return counter
 }
 
+// AddCounterVec creates a new CounterVec based on the provided CounterOpts and
+// partitioned by the given label names which will be registered automatically
+// when client serves.
 func (c *Client) AddCounterVec(
 	opts prometheus.CounterOpts, labelNames []string,
 ) *prometheus.CounterVec {
@@ -26,6 +31,8 @@ func (c *Client) AddCounterVec(
 	return counterVec
 }
 
+// AddGauge creates a new Gauge based on the provided GaugeOpts which will be
+// registered automatically when client serves.
 func (c *Client) AddGauge(opts prometheus.GaugeOpts) prometheus.Gauge {
 	opts = prometheus.GaugeOpts(
 		formatOpts(prometheus.Opts(opts), c.ConstLabels, c.ServiceName),
@@ -36,6 +43,9 @@ func (c *Client) AddGauge(opts prometheus.GaugeOpts) prometheus.Gauge {
 	return gauge
 }
 
+// AddGaugeVec creates a new GaugeVec based on the provided GaugeOpts and
+// partitioned by the given label names which will be registered automatically
+// when client serves.
 func (c *Client) AddGaugeVec(
 	opts prometheus.GaugeOpts, labelNames []string,
 ) *prometheus.GaugeVec {
@@ -48,6 +58,8 @@ func (c *Client) AddGaugeVec(
 	return gaugeVec
 }
 
+// AddHistogram creates a new Histogram based on the provided HistogramOpts
+// which will be registered automatically when client serves.
 func (c *Client) AddHistogram(opts prometheus.HistogramOpts) prometheus.Histogram {
 	opts = formatHistogramOpts(opts, c.ConstLabels, c.ServiceName)
 
@@ -56,6 +68,9 @@ func (c *Client) AddHistogram(opts prometheus.HistogramOpts) prometheus.Histogra
 	return histogram
 }
 
+// AddHistogramVec creates a new HistogramVec based on the provided
+// HistogramOpts and partitioned by the given label names which will be
+// registered automatically when client serves.
 func (c *Client) AddHistogramVec(
 	opts prometheus.HistogramOpts, labelNames []string,
 ) *prometheus.HistogramVec {
@@ -66,6 +81,8 @@ func (c *Client) AddHistogramVec(
 	return histogramVec
 }
 
+// AddSummary creates a new Summary based on the provided SummaryOpts which will
+// be registered automatically when client serves.
 func (c *Client) AddSummary(opts prometheus.SummaryOpts) prometheus.Summary {
 	opts = formatSummaryOpts(opts, c.ConstLabels, c.ServiceName)
 
@@ -74,6 +91,9 @@ func (c *Client) AddSummary(opts prometheus.SummaryOpts) prometheus.Summary {
 	return summary
 }
 
+// AddSummaryVec creates a new SummaryVec based on the provided SummaryOpts and
+// partitioned by the given label names which will be registered automatically
+// when client serves.
 func (c *Client) AddSummaryVec(
 	opts prometheus.SummaryOpts, labelNames []string,
 ) *prometheus.SummaryVec {
